d6892cafefa7afd27e2ee9a9d072f4a4f036d3ac
isaiah <isaiya.mwamburi@student.moringaschool.com>
2026-04-17 19:23:03 +0300

    compeleted HTML Forms- Tasklister Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+const taskForm = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task-input');
+const taskList = document.querySelector('#task-list');
+
+taskForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // Prevents the page from refreshing
+
+  const newTaskValue = taskInput.value;
+  
+  if (newTaskValue.trim() !== "") {
+    addItemToList(newTaskValue);
+    taskForm.reset(); // Clears the input field
+  }
+});
+
 });
